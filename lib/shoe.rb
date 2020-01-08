9d3296e40d8b911608dfc60e8eebc8809648d097
Meghan Flynn <mflynn333@gmail.com>
2020-01-08 22:43:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,6 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    @brand = brand
-  end
-  
-  
-  def brand=(brand)
     @brand = brand 
     BRANDS << brand unless BRANDS.include?(brand)
   end
